aee7d3c5f7d017906b35853a15bf325b11cd36a0
MrNeverWasteTime <amartin719@boscdelacoma.cat>
2025-02-17 19:33:06 +0100

    Update Joc.py

--- Joc.py
+++ Joc.py
@@ -19,3 +19,51 @@ bg = pygame.image.load("https://www.google.com/imgres?q=dank%20memer&imgurl=http
 player_img = pygame.image.load("player.png")
 enemy_img = pygame.image.load("enemy.png")
 bullet_img = pygame.image.load("bullet.png")
+
+# Classes
+class Player:
+    def __init__(self):
+        self.image = pygame.transform.scale(player_img, (50, 50))
+        self.x = WIDTH // 2
+        self.y = HEIGHT - 80
+        self.speed = 5
+        self.lives = 3
+
+    def draw(self):
+        screen.blit(self.image, (self.x, self.y))
+
+    def move(self, direction):
+        if direction == "left" and self.x > 0:
+            self.x -= self.speed
+        if direction == "right" and self.x < WIDTH - 50:
+            self.x += self.speed
+
+class Enemy:
+    def __init__(self):
+        self.image = pygame.transform.scale(enemy_img, (40, 40))
+        self.x = random.randint(0, WIDTH - 40)
+        self.y = random.randint(-100, -40)
+        self.speed = random.randint(2, 4)
+
+    def move(self):
+        self.y += self.speed
+        if self.y > HEIGHT:
+            self.y = random.randint(-100, -40)
+            self.x = random.randint(0, WIDTH - 40)
+
+    def draw(self):
+        screen.blit(self.image, (self.x, self.y))
+
+class Bullet:
+    def __init__(self, x, y):
+        self.image = pygame.transform.scale(bullet_img, (10, 20))
+        self.x = x
+        self.y = y
+        self.speed = 7
+
+    def move(self):
+        self.y -= self.speed
+
+    def draw(self):
+        screen.blit(self.image, (self.x, self.y))
+
